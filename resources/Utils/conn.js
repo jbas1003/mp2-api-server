cf154b6e87a84a200395d66a6c3667f23a9a62b9
Jon <joncaesar@gmail.com>
2023-02-20 16:37:03 +0800

    Update conn.js

--- resources/Utils/conn.js
+++ resources/Utils/conn.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 export const newSequelize = Sequelize
-export const taskSupportDb = new Sequelize('taskSupportdb', 'root', '12345', {
-    host: 'localhost',
+export const taskSupportDb = new Sequelize('Basco', 'wd32p', '7YWFvP8kFyHhG3eF', {
+    host: '20.211.37.87',
     dialect: 'mysql'
 });
